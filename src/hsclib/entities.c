bfc5dd357dd0a919967f27d425c21a1758ff615e
Matthias Bethke <matthias@towiski.de>
2003-07-06 04:11:08 +0000

    Removed superfluous replacement character entries; added prefnum flag

--- src/hsclib/entities.c
+++ src/hsclib/entities.c
@@ -3,98 +3,100 @@
 static struct HSCENTITY {
    STRPTR name;
    unsigned short numeric;
-   unsigned char replace;
+   BOOL prefnum;
 } HSCInternalEntities[] = {
    /* basic entities w/o replacement chars */
-   {"amp",     0, '\0'},  /* & */
-   {"lt",      0, '\0'},  /* < */
-   {"gt",      0, '\0'},  /* > */
-   {"quot",    0, '\0'},  /* " */
-   /* ISO-8859-1 entities stay in the prefs file! */
+   {"amp",     0, FALSE},  /* & */
+   {"lt",      0, FALSE},  /* < */
+   {"gt",      0, FALSE},  /* > */
+   {"quot",    0, FALSE},  /* " */
+   /* ISO-8859-1 entities stay in the prefs file to allow users to utilize
+    * different 8-bit charsets! */
 
-   /* UNICODE entities that don't have a corresponding character in Latin-1 */
-   {"ensp",		8194, 0}, 	/* en space (1/2-em) */
-   {"emsp",		8195, 0}, 	/* em space */
-   {"emsp13",	8196, 0}, 	/* 1/3-em space */
-   {"emsp14",	8197, 0}, 	/* 1/4-em space */
-   {"numsp",	8199, 0}, 	/* digit space (width of a number) */
-   {"puncsp",	8200, 0}, 	/* punctuation space (width of comma) */
-   {"thinsp",	8201, 0}, 	/* thin space (1/6-em) */
-   {"hairsp",	8202, 0}, 	/* hair space */
-   {"dash",		8208, 0}, 	/* hyphen (true graphic) */
-   {"ndash",	8211, 0}, 	/* en dash */
-   {"mdash",	8212, 0}, 	/* em dash */
-   {"rsquor",	8217, 0}, 	/* rising single quote, right (high) */
-   {"lsquor",	8218, 0}, 	/* rising single quote, left (low) */
-   {"rdquor",	8221, 0}, 	/* rising dbl quote, right (high) */
-   {"ldquor",	8222, 0}, 	/* rising dbl quote, left (low) */
-   {"dagger",	8224, 0}, 	/* dagger */
-   {"Dagger",	8225, 0}, 	/* double dagger */
-   {"bull",		8226, 0}, 	/* round bullet, filled */
-   {"nldr",		8229, 0}, 	/* double baseline dot (en leader) */
-   {"hellip",	8230, 0}, 	/* ellipsis (horizontal) */
-   {"caret",	8257, 0}, 	/* caret (insertion mark) */
-   {"hybull",	8259, 0}, 	/* rectangle, filled (hyphen bullet) */
-   {"incare",	8453, 0}, 	/* in-care-of symbol */
-   {"copysr",	8471, 0}, 	/* sound recording copyright sign */
-   {"rx",		8478, 0}, 	/* pharmaceutical prescription (Rx) */
-   {"frac13",	8531, 0}, 	/* fraction one-third */
-   {"frac23",	8532, 0}, 	/* fraction two-thirds */
-   {"frac15",	8533, 0}, 	/* fraction one-fifth */
-   {"frac25",	8534, 0}, 	/* fraction two-fifths */
-   {"frac35",	8535, 0}, 	/* fraction three-fifths */
-   {"frac45",	8536, 0}, 	/* fraction four-fifths */
-   {"frac16",	8537, 0}, 	/* fraction one-sixth */
-   {"frac56",	8538, 0}, 	/* fraction five-sixths */
-   {"vellip",	8942, 0}, 	/* vertical ellipsis */
-   {"drcrop",	8972, 0}, 	/* downward right crop mark  */
-   {"dlcrop",	8973, 0}, 	/* downward left crop mark  */
-   {"urcrop",	8974, 0}, 	/* upward right crop mark  */
-   {"ulcrop",	8975, 0}, 	/* upward left crop mark  */
-   {"telrec",	8981, 0}, 	/* telephone recorder symbol */
-   {"target",	8982, 0}, 	/* register mark or target */
-   {"blank",	9251, 0}, 	/* significant blank symbol */
-   {"uhblk",	9600, 0}, 	/* upper half block */
-   {"lhblk",	9604, 0}, 	/* lower half block */
-   {"block",	9608, 0}, 	/* full block */
-   {"blk14",	9617, 0}, 	/* 25haded block */
-   {"blk12",	9618, 0}, 	/* 50haded block */
-   {"blk34",	9619, 0}, 	/* 75haded block */
-   {"squ",		9633, 0}, 	/* square, open */
-   {"squf",		9642, 0}, 	/* sq bullet, filled */
-   {"rect",		9645, 0}, 	/* rectangle, open */
-   {"marker",	9646, 0}, 	/* histogram marker */
-   {"utrif",	9652, 0}, 	/* up tri, filled */
-   {"utri",		9653, 0}, 	/* up triangle, open */
-   {"rtrif",	9656, 0}, 	/* r tri, filled */
-   {"rtri",		9657, 0}, 	/* /triangleright B: r triangle, open */
-   {"dtrif",	9662, 0}, 	/* dn tri, filled */
-   {"dtri",		9663, 0}, 	/* down triangle, open */
-   {"ltrif",	9666, 0}, 	/* l tri, filled */
-   {"ltri",		9667, 0}, 	/* /triangleleft B: l triangle, open */
-   {"cir",		9675, 0}, 	/* circle, open */
-   {"starf",	9733, 0}, 	/* /bigstar - star, filled */
-   {"star",		9734, 0}, 	/* star, open */
-   {"female",	9792, 0}, 	/* female symbol */
-   {"male",		9794, 0}, 	/* male symbol */
-   {"spades",	9824, 0}, 	/* spades suit symbol */
-   {"hearts",	9825, 0}, 	/* heart suit symbol */
-   {"diams",	9826, 0}, 	/* diamond suit symbol */
-   {"clubs",	9827, 0}, 	/* club suit symbol */
-   {"flat",		9837, 0}, 	/* musical flat */
-   {"natur",	9838, 0}, 	/* /natural - music natural */
-   {"sharp",	9839, 0}, 	/* musical sharp */
-   {"phone",	9952, 0}, 	/* telephone symbol */
-   {"check",	10003,0},	/* tick, check mark */
-   {"cross",	10007,0},	/* ballot cross */
-   {"malt",		10016,0},	/* maltese cross */
-   {"lozf",		10022,0},	/* /blacklozenge - lozenge, filled */
-   {"loz",		10023,0},	/* /lozenge - lozenge or total mark */
-   {"sext",		10038,0},	/* sextile (6-pointed star) */
-   {"fflig",	64256,0},	/* small ff ligature */
-   {"filig",	64257,0},	/* small fi ligature */
-   {"fllig",	64258,0},	/* small fl ligature */
-   {"ffilig",	64259,0},	/* small ffi ligature */
-   {"ffllig",	64260,0},	/* small ffl ligature */
-   {"fjlig",	65533,0} 	/* small fj ligature */
+   /* UNICODE entities that don't have a corresponding character
+    * in an 8-bit charset */
+   {"ensp",		8194, FALSE}, 	/* en space (1/2-em) */
+   {"emsp",		8195, FALSE}, 	/* em space */
+   {"emsp13",	8196, FALSE}, 	/* 1/3-em space */
+   {"emsp14",	8197, FALSE}, 	/* 1/4-em space */
+   {"numsp",	8199, FALSE}, 	/* digit space (width of a number) */
+   {"puncsp",	8200, FALSE}, 	/* punctuation space (width of comma) */
+   {"thinsp",	8201, FALSE}, 	/* thin space (1/6-em) */
+   {"hairsp",	8202, FALSE}, 	/* hair space */
+   {"dash",		8208, FALSE}, 	/* hyphen (true graphic) */
+   {"ndash",	8211, FALSE}, 	/* en dash */
+   {"mdash",	8212, FALSE}, 	/* em dash */
+   {"rsquor",	8217, FALSE}, 	/* rising single quote, right (high) */
+   {"lsquor",	8218, FALSE}, 	/* rising single quote, left (low) */
+   {"rdquor",	8221, FALSE}, 	/* rising dbl quote, right (high) */
+   {"ldquor",	8222, FALSE}, 	/* rising dbl quote, left (low) */
+   {"dagger",	8224, FALSE}, 	/* dagger */
+   {"Dagger",	8225, FALSE}, 	/* double dagger */
+   {"bull",		8226, FALSE}, 	/* round bullet, filled */
+   {"nldr",		8229, FALSE}, 	/* double baseline dot (en leader) */
+   {"hellip",	8230, FALSE}, 	/* ellipsis (horizontal) */
+   {"caret",	8257, FALSE}, 	/* caret (insertion mark) */
+   {"hybull",	8259, FALSE}, 	/* rectangle, filled (hyphen bullet) */
+   {"incare",	8453, FALSE}, 	/* in-care-of symbol */
+   {"copysr",	8471, FALSE}, 	/* sound recording copyright sign */
+   {"rx",		8478, FALSE}, 	/* pharmaceutical prescription (Rx) */
+   {"frac13",	8531, FALSE}, 	/* fraction one-third */
+   {"frac23",	8532, FALSE}, 	/* fraction two-thirds */
+   {"frac15",	8533, FALSE}, 	/* fraction one-fifth */
+   {"frac25",	8534, FALSE}, 	/* fraction two-fifths */
+   {"frac35",	8535, FALSE}, 	/* fraction three-fifths */
+   {"frac45",	8536, FALSE}, 	/* fraction four-fifths */
+   {"frac16",	8537, FALSE}, 	/* fraction one-sixth */
+   {"frac56",	8538, FALSE}, 	/* fraction five-sixths */
+   {"vellip",	8942, FALSE}, 	/* vertical ellipsis */
+   {"drcrop",	8972, FALSE}, 	/* downward right crop mark  */
+   {"dlcrop",	8973, FALSE}, 	/* downward left crop mark  */
+   {"urcrop",	8974, FALSE}, 	/* upward right crop mark  */
+   {"ulcrop",	8975, FALSE}, 	/* upward left crop mark  */
+   {"telrec",	8981, FALSE}, 	/* telephone recorder symbol */
+   {"target",	8982, FALSE}, 	/* register mark or target */
+   {"blank",	9251, FALSE}, 	/* significant blank symbol */
+   {"uhblk",	9600, FALSE}, 	/* upper half block */
+   {"lhblk",	9604, FALSE}, 	/* lower half block */
+   {"block",	9608, FALSE}, 	/* full block */
+   {"blk14",	9617, FALSE}, 	/* 25haded block */
+   {"blk12",	9618, FALSE}, 	/* 50haded block */
+   {"blk34",	9619, FALSE}, 	/* 75haded block */
+   {"squ",		9633, FALSE}, 	/* square, open */
+   {"squf",		9642, FALSE}, 	/* sq bullet, filled */
+   {"rect",		9645, FALSE}, 	/* rectangle, open */
+   {"marker",	9646, FALSE}, 	/* histogram marker */
+   {"utrif",	9652, FALSE}, 	/* up tri, filled */
+   {"utri",		9653, FALSE}, 	/* up triangle, open */
+   {"rtrif",	9656, FALSE}, 	/* r tri, filled */
+   {"rtri",		9657, FALSE}, 	/* /triangleright B: r triangle, open */
+   {"dtrif",	9662, FALSE}, 	/* dn tri, filled */
+   {"dtri",		9663, FALSE}, 	/* down triangle, open */
+   {"ltrif",	9666, FALSE}, 	/* l tri, filled */
+   {"ltri",		9667, FALSE}, 	/* /triangleleft B: l triangle, open */
+   {"cir",		9675, FALSE}, 	/* circle, open */
+   {"starf",	9733, FALSE}, 	/* /bigstar - star, filled */
+   {"star",		9734, FALSE}, 	/* star, open */
+   {"female",	9792, FALSE}, 	/* female symbol */
+   {"male",		9794, FALSE}, 	/* male symbol */
+   {"spades",	9824, FALSE}, 	/* spades suit symbol */
+   {"hearts",	9825, FALSE}, 	/* heart suit symbol */
+   {"diams",	9826, FALSE}, 	/* diamond suit symbol */
+   {"clubs",	9827, FALSE}, 	/* club suit symbol */
+   {"flat",		9837, FALSE}, 	/* musical flat */
+   {"natur",	9838, FALSE}, 	/* /natural - music natural */
+   {"sharp",	9839, FALSE}, 	/* musical sharp */
+   {"phone",	9952, FALSE}, 	/* telephone symbol */
+   {"check",	10003,FALSE},	/* tick, check mark */
+   {"cross",	10007,FALSE},	/* ballot cross */
+   {"malt",		10016,FALSE},	/* maltese cross */
+   {"lozf",		10022,FALSE},	/* /blacklozenge - lozenge, filled */
+   {"loz",		10023,FALSE},	/* /lozenge - lozenge or total mark */
+   {"sext",		10038,FALSE},	/* sextile (6-pointed star) */
+   {"fflig",	64256,FALSE},	/* small ff ligature */
+   {"filig",	64257,FALSE},	/* small fi ligature */
+   {"fllig",	64258,FALSE},	/* small fl ligature */
+   {"ffilig",	64259,FALSE},	/* small ffi ligature */
+   {"ffllig",	64260,FALSE},	/* small ffl ligature */
+   {"fjlig",	65533,FALSE} 	/* small fj ligature */
 };
