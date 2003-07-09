92783db2e5efecafca91689b8135b28a10ed7df6
Matthias Bethke <matthias@towiski.de>
2003-07-09 14:36:35 +0000

    Replaced with official XHTML entity list

--- src/hsclib/entities.c
+++ src/hsclib/entities.c
@@ -10,95 +10,162 @@ static struct HSCENTITY {
    {"lt",      0, FALSE},  /* < */
    {"gt",      0, FALSE},  /* > */
    {"quot",    0, FALSE},  /* " */
+   {"apos",    0, FALSE},  /* ' */
+
    /* ISO-8859-1 entities stay in the prefs file to allow users to utilize
     * different 8-bit charsets! */
 
-   /* UNICODE entities that don't have a corresponding character
-    * in an 8-bit charset */
-   {"ensp",		8194, FALSE}, 	/* en space (1/2-em) */
-   {"emsp",		8195, FALSE}, 	/* em space */
-   {"emsp13",	8196, FALSE}, 	/* 1/3-em space */
-   {"emsp14",	8197, FALSE}, 	/* 1/4-em space */
-   {"numsp",	8199, FALSE}, 	/* digit space (width of a number) */
-   {"puncsp",	8200, FALSE}, 	/* punctuation space (width of comma) */
-   {"thinsp",	8201, FALSE}, 	/* thin space (1/6-em) */
-   {"hairsp",	8202, FALSE}, 	/* hair space */
-   {"dash",		8208, FALSE}, 	/* hyphen (true graphic) */
-   {"ndash",	8211, FALSE}, 	/* en dash */
-   {"mdash",	8212, FALSE}, 	/* em dash */
-   {"rsquor",	8217, FALSE}, 	/* rising single quote, right (high) */
-   {"lsquor",	8218, FALSE}, 	/* rising single quote, left (low) */
-   {"rdquor",	8221, FALSE}, 	/* rising dbl quote, right (high) */
-   {"ldquor",	8222, FALSE}, 	/* rising dbl quote, left (low) */
-   {"dagger",	8224, FALSE}, 	/* dagger */
-   {"Dagger",	8225, FALSE}, 	/* double dagger */
-   {"bull",		8226, FALSE}, 	/* round bullet, filled */
-   {"nldr",		8229, FALSE}, 	/* double baseline dot (en leader) */
-   {"hellip",	8230, FALSE}, 	/* ellipsis (horizontal) */
-   {"caret",	8257, FALSE}, 	/* caret (insertion mark) */
-   {"hybull",	8259, FALSE}, 	/* rectangle, filled (hyphen bullet) */
-   {"incare",	8453, FALSE}, 	/* in-care-of symbol */
-   {"copysr",	8471, FALSE}, 	/* sound recording copyright sign */
-   {"rx",		8478, FALSE}, 	/* pharmaceutical prescription (Rx) */
-   {"frac13",	8531, FALSE}, 	/* fraction one-third */
-   {"frac23",	8532, FALSE}, 	/* fraction two-thirds */
-   {"frac15",	8533, FALSE}, 	/* fraction one-fifth */
-   {"frac25",	8534, FALSE}, 	/* fraction two-fifths */
-   {"frac35",	8535, FALSE}, 	/* fraction three-fifths */
-   {"frac45",	8536, FALSE}, 	/* fraction four-fifths */
-   {"frac16",	8537, FALSE}, 	/* fraction one-sixth */
-   {"frac56",	8538, FALSE}, 	/* fraction five-sixths */
-   {"vellip",	8942, FALSE}, 	/* vertical ellipsis */
-   {"drcrop",	8972, FALSE}, 	/* downward right crop mark  */
-   {"dlcrop",	8973, FALSE}, 	/* downward left crop mark  */
-   {"urcrop",	8974, FALSE}, 	/* upward right crop mark  */
-   {"ulcrop",	8975, FALSE}, 	/* upward left crop mark  */
-   {"telrec",	8981, FALSE}, 	/* telephone recorder symbol */
-   {"target",	8982, FALSE}, 	/* register mark or target */
-   {"blank",	9251, FALSE}, 	/* significant blank symbol */
-   {"uhblk",	9600, FALSE}, 	/* upper half block */
-   {"lhblk",	9604, FALSE}, 	/* lower half block */
-   {"block",	9608, FALSE}, 	/* full block */
-   {"blk14",	9617, FALSE}, 	/* 25haded block */
-   {"blk12",	9618, FALSE}, 	/* 50haded block */
-   {"blk34",	9619, FALSE}, 	/* 75haded block */
-   {"squ",		9633, FALSE}, 	/* square, open */
-   {"squf",		9642, FALSE}, 	/* sq bullet, filled */
-   {"rect",		9645, FALSE}, 	/* rectangle, open */
-   {"marker",	9646, FALSE}, 	/* histogram marker */
-   {"utrif",	9652, FALSE}, 	/* up tri, filled */
-   {"utri",		9653, FALSE}, 	/* up triangle, open */
-   {"rtrif",	9656, FALSE}, 	/* r tri, filled */
-   {"rtri",		9657, FALSE}, 	/* /triangleright B: r triangle, open */
-   {"dtrif",	9662, FALSE}, 	/* dn tri, filled */
-   {"dtri",		9663, FALSE}, 	/* down triangle, open */
-   {"ltrif",	9666, FALSE}, 	/* l tri, filled */
-   {"ltri",		9667, FALSE}, 	/* /triangleleft B: l triangle, open */
-   {"cir",		9675, FALSE}, 	/* circle, open */
-   {"starf",	9733, FALSE}, 	/* /bigstar - star, filled */
-   {"star",		9734, FALSE}, 	/* star, open */
-   {"female",	9792, FALSE}, 	/* female symbol */
-   {"male",		9794, FALSE}, 	/* male symbol */
-   {"spades",	9824, FALSE}, 	/* spades suit symbol */
-   {"hearts",	9825, FALSE}, 	/* heart suit symbol */
-   {"diams",	9826, FALSE}, 	/* diamond suit symbol */
-   {"clubs",	9827, FALSE}, 	/* club suit symbol */
-   {"flat",		9837, FALSE}, 	/* musical flat */
-   {"natur",	9838, FALSE}, 	/* /natural - music natural */
-   {"sharp",	9839, FALSE}, 	/* musical sharp */
-   {"phone",	9952, FALSE}, 	/* telephone symbol */
-   {"check",	10003,FALSE},	/* tick, check mark */
-   {"cross",	10007,FALSE},	/* ballot cross */
-   {"malt",		10016,FALSE},	/* maltese cross */
-   {"lozf",		10022,FALSE},	/* /blacklozenge - lozenge, filled */
-   {"loz",		10023,FALSE},	/* /lozenge - lozenge or total mark */
-   {"sext",		10038,FALSE},	/* sextile (6-pointed star) */
-   {"fflig",	64256,FALSE},	/* small ff ligature */
-   {"filig",	64257,FALSE},	/* small fi ligature */
-   {"fllig",	64258,FALSE},	/* small fl ligature */
-   {"ffilig",	64259,FALSE},	/* small ffi ligature */
-   {"ffllig",	64260,FALSE},	/* small ffl ligature */
-   {"fjlig",	65533,FALSE} 	/* small fj ligature */
+	{"OElig",	338, FALSE},
+ 	{"oelig",	339, FALSE},
+ 	{"Scaron",	352, FALSE},
+ 	{"scaron",	353, FALSE},
+ 	{"Yuml",	376, FALSE},
+ 	{"circ",	710, FALSE},
+ 	{"tilde",	732, FALSE},
+ 	{"ensp",	8194, FALSE},
+ 	{"emsp",	8195, FALSE},
+ 	{"thinsp",	8201, FALSE},
+ 	{"zwnj",	8204, FALSE},
+ 	{"zwj",	8205, FALSE},
+ 	{"lrm",	8206, FALSE},
+ 	{"rlm",	8207, FALSE},
+ 	{"ndash",	8211, FALSE},
+ 	{"mdash",	8212, FALSE},
+ 	{"lsquo",	8216, FALSE},
+ 	{"rsquo",	8217, FALSE},
+ 	{"sbquo",	8218, FALSE},
+ 	{"ldquo",	8220, FALSE},
+ 	{"rdquo",	8221, FALSE},
+ 	{"bdquo",	8222, FALSE},
+ 	{"dagger",	8224, FALSE},
+ 	{"Dagger",	8225, FALSE},
+ 	{"permil",	8240, FALSE},
+ 	{"lsaquo",	8249, FALSE},
+ 	{"rsaquo",	8250, FALSE},
+ 	{"euro",	8364, FALSE},
+ 	{"fnof",	402, FALSE},
+ 	{"Alpha",	913, FALSE},
+ 	{"Beta",	914, FALSE},
+ 	{"Gamma",	915, FALSE},
+ 	{"Delta",	916, FALSE},
+ 	{"Epsilon",	917, FALSE},
+ 	{"Zeta",	918, FALSE},
+ 	{"Eta",	919, FALSE},
+ 	{"Theta",	920, FALSE},
+ 	{"Iota",	921, FALSE},
+ 	{"Kappa",	922, FALSE},
+ 	{"Lambda",	923, FALSE},
+ 	{"Mu",	924, FALSE},
+ 	{"Nu",	925, FALSE},
+ 	{"Xi",	926, FALSE},
+ 	{"Omicron",	927, FALSE},
+ 	{"Pi",	928, FALSE},
+ 	{"Rho",	929, FALSE},
+ 	{"Sigma",	931, FALSE},
+ 	{"Tau",	932, FALSE},
+ 	{"Upsilon",	933, FALSE},
+ 	{"Phi",	934, FALSE},
+ 	{"Chi",	935, FALSE},
+ 	{"Psi",	936, FALSE},
+ 	{"Omega",	937, FALSE},
+ 	{"alpha",	945, FALSE},
+ 	{"beta",	946, FALSE},
+ 	{"gamma",	947, FALSE},
+ 	{"delta",	948, FALSE},
+ 	{"epsilon",	949, FALSE},
+ 	{"zeta",	950, FALSE},
+ 	{"eta",	951, FALSE},
+ 	{"theta",	952, FALSE},
+ 	{"iota",	953, FALSE},
+ 	{"kappa",	954, FALSE},
+ 	{"lambda",	955, FALSE},
+ 	{"mu",	956, FALSE},
+ 	{"nu",	957, FALSE},
+ 	{"xi",	958, FALSE},
+ 	{"omicron",	959, FALSE},
+ 	{"pi",	960, FALSE},
+ 	{"rho",	961, FALSE},
+ 	{"sigmaf",	962, FALSE},
+ 	{"sigma",	963, FALSE},
+ 	{"tau",	964, FALSE},
+ 	{"upsilon",	965, FALSE},
+ 	{"phi",	966, FALSE},
+ 	{"chi",	967, FALSE},
+ 	{"psi",	968, FALSE},
+ 	{"omega",	969, FALSE},
+ 	{"thetasym",	977, FALSE},
+ 	{"upsih",	978, FALSE},
+ 	{"piv",	982, FALSE},
+ 	{"bull",	8226, FALSE},
+ 	{"hellip",	8230, FALSE},
+ 	{"prime",	8242, FALSE},
+ 	{"Prime",	8243, FALSE},
+ 	{"oline",	8254, FALSE},
+ 	{"frasl",	8260, FALSE},
+ 	{"weierp",	8472, FALSE},
+ 	{"image",	8465, FALSE},
+ 	{"real",	8476, FALSE},
+ 	{"trade",	8482, FALSE},
+ 	{"alefsym",	8501, FALSE},
+ 	{"larr",	8592, FALSE},
+ 	{"uarr",	8593, FALSE},
+ 	{"darr",	8595, FALSE},
+ 	{"harr",	8596, FALSE},
+ 	{"crarr",	8629, FALSE},
+ 	{"lArr",	8656, FALSE},
+ 	{"uArr",	8657, FALSE},
+ 	{"rArr",	8658, FALSE},
+ 	{"dArr",	8659, FALSE},
+ 	{"hArr",	8660, FALSE},
+ 	{"forall",	8704, FALSE},
+ 	{"part",	8706, FALSE},
+ 	{"exist",	8707, FALSE},
+ 	{"empty",	8709, FALSE},
+ 	{"nabla",	8711, FALSE},
+ 	{"isin",	8712, FALSE},
+ 	{"notin",	8713, FALSE},
+ 	{"ni",	8715, FALSE},
+ 	{"prod",	8719, FALSE},
+ 	{"sum",	8721, FALSE},
+ 	{"minus",	8722, FALSE},
+ 	{"lowast",	8727, FALSE},
+ 	{"radic",	8730, FALSE},
+ 	{"prop",	8733, FALSE},
+ 	{"infin",	8734, FALSE},
+ 	{"ang",	8736, FALSE},
+ 	{"and",	8743, FALSE},
+ 	{"or",	8744, FALSE},
+ 	{"cap",	8745, FALSE},
+ 	{"cup",	8746, FALSE},
+ 	{"int",	8747, FALSE},
+ 	{"there4",	8756, FALSE},
+ 	{"sim",	8764, FALSE},
+ 	{"cong",	8773, FALSE},
+ 	{"asymp",	8776, FALSE},
+ 	{"ne",	8800, FALSE},
+ 	{"equiv",	8801, FALSE},
+ 	{"le",	8804, FALSE},
+ 	{"ge",	8805, FALSE},
+ 	{"sub",	8834, FALSE},
+ 	{"sup",	8835, FALSE},
+ 	{"nsub",	8836, FALSE},
+ 	{"sube",	8838, FALSE},
+ 	{"supe",	8839, FALSE},
+ 	{"oplus",	8853, FALSE},
+ 	{"otimes",	8855, FALSE},
+ 	{"perp",	8869, FALSE},
+ 	{"sdot",	8901, FALSE},
+ 	{"lceil",	8968, FALSE},
+ 	{"rceil",	8969, FALSE},
+ 	{"lfloor",	8970, FALSE},
+ 	{"rfloor",	8971, FALSE},
+ 	{"lang",	9001, FALSE},
+ 	{"rang",	9002, FALSE},
+ 	{"loz",	9674, FALSE},
+ 	{"spades",	9824, FALSE},
+ 	{"clubs",	9827, FALSE},
+ 	{"hearts",	9829, FALSE},
+ 	{"diams",	9830, FALSE}
 };
 
 /* $Id$ */
