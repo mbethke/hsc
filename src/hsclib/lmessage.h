734c7b51ac07bc619c3dbbf1f4bab96cc2bb10a6
Matthias Bethke <matthias@towiski.de>
2004-02-05 13:40:07 +0000

    Removed enforcerHit() prototype

--- src/hsclib/lmessage.h
+++ src/hsclib/lmessage.h
@@ -48,8 +48,6 @@ extern VOID hsc_msg_eol(HSCPRC * hp);
 extern VOID hsc_msg_noinput(HSCPRC * hp, STRPTR filename);
 extern VOID hsc_msg_nouri(HSCPRC * hp, STRPTR filename, STRPTR uriname, STRPTR note);
 
-extern VOID enforcerHit(VOID);
-
 #endif /* NOEXTERN_HSCLIB_MESSAGE */
 #endif /* HSCLIB_MESSAGE_H */
 
